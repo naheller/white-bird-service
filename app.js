8d4a37a35b66225c6e5cc573d87df907d522fe05
Nathan Heller <naheller@gmail.com>
2019-11-10 14:09:31 -0800

    Connect remote db

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
 const express = require('express')
-// const cors = require('cors')
+const cors = require('cors')
 const mongoose = require('mongoose')
 const organizations = require('./routes/organization')
 const port = process.env.PORT || 5000
 
 mongoose
-  .connect('mongodb://localhost:27017/whitebird', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
+  .connect(
+    `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@ds042908.mlab.com:42908/whitebird`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    }
+  )
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('Error connecting to MongoDB:', err))
 
 const app = express()
-// app.use(cors())
+app.use(cors())
 // app.use(express.json())
 
 app.use('/api/organizations', organizations)
